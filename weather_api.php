8db4b7b81bd60757769dfeca0d5781cf60cddc6f
alexmozzhakov <alexey.m@ex.ua>
2016-06-22 17:41:50 +0300

    Try to fix

--- weather_api.php
+++ weather_api.php
@@ -5,7 +5,7 @@ curl_setopt($ch, CURLOPT_URL, "ip-api.com/json/" . $_SERVER['REMOTE_ADDR']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $locationData = json_decode(curl_exec($ch));
 curl_close($ch);
-$yql_query = 'select item.condition.temp from weather.forecast where woeid in (select woeid from geo.places(1) where text="' . $locationData->city . ', ' . $locationData->country . '")and u="c"';
+$yql_query = 'SELECT item.condition.temp FROM weather.forecast WHERE woeid IN (SELECT woeid FROM geo.places(1) WHERE TEXT="' . $locationData->city . ', ' . $locationData->country . '")AND u="c"';
 $yql_query_url = "http://query.yahooapis.com/v1/public/yql?q=" . urlencode($yql_query) . "&format=json";
 $session = curl_init($yql_query_url);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
@@ -13,9 +13,8 @@ $json = curl_exec($session);
 curl_close($session);
 $arr = json_decode($json);
 $c = $arr->query->results->channel->item->condition->temp;
-if ($c != 0) {
-    print_r('{"celsius" : "' . $c . '","location" : "' . $locationData->city . ', ' . $locationData->country . '"');
-} else print_r('{"error" : "Can\'t get weather"');
-// print_r("}");
-$time_elapsed_secs = (microtime(true) - $start)*100;
+// if ($c != 0) {
+print_r('{"celsius" : "' . $c . '","location" : "' . $locationData->city . ', ' . $locationData->country . '"');
+// } else print_r('{"error" : "Can\'t get weather"');
+$time_elapsed_secs = (microtime(true) - $start) * 100;
 print_r(', "time_elapsed":"' . $time_elapsed_secs . ' ms" }');
